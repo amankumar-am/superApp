b29636452b6f0b96700a639ae76858e6d0be5312
amankumar-am <aman.kumar@aditmicrosys.com>
2025-03-21 11:33:37 +0530

    updated dashboard component from about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../Components/home/home.component';
-import { AboutComponent } from '../Components/about/about.component';
+import { DashboardComponent } from '../Components/dashboard/dashboard.component';
 import { ContactComponent } from '../Components/contact/contact.component';
-import { ProductsComponent } from '../Components/products/products.component';
 import { AddProductComponent } from '../Components/products/add-product/add-product.component';
 import { ViewProductComponent } from '../Components/products/view-product/view-product.component';
 import { NgModule } from '@angular/core';
@@ -10,7 +9,7 @@ import { NgModule } from '@angular/core';
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
+    { path: 'dashboard', component: DashboardComponent },
     { path: 'products/add', component: AddProductComponent },
     { path: 'products/view', component: ViewProductComponent },
     { path: 'contact', component: ContactComponent },
